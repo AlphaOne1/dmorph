43dbf19aa8966c1de1f8b24e9b4ff63c17b603dc
Alexander Adam <alphaone23@gmail.com>
2025-06-09 01:39:30 +0200

    Add DialectCSVQ implementation for CSVQ databases

---
+++ dialect_csvq.go
@@ -0,0 +1,22 @@
+// Copyright the DMorph contributors.
+// SPDX-License-Identifier: MPL-2.0
+
+package dmorph
+
+// DialectCSVQ returns a Dialect configured for CSVQ databases.
+func DialectCSVQ() BaseDialect {
+	return BaseDialect{
+		CreateTemplate: `
+			CREATE TABLE IF NOT EXISTS %s (
+				id,
+				create_ts
+			)`,
+		AppliedTemplate: `
+			SELECT id
+			FROM   %s
+	        ORDER BY create_ts ASC`,
+		RegisterTemplate: `
+			INSERT INTO %s (id)
+	        VALUES(:id)`,
+	}
+}
